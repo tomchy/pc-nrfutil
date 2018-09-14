7500896705ce534abcfc4886fd0fd1dad72e6f12
Frank Audun Kvamtrø <frank.kvamtro@nordicsemi.no>
2018-09-14 17:25:37 +0200

    NRF-1904: Allowing --sd-req to not be set for --external_app NRF-1904: Fixing boolean error for --external_app

--- nordicsemi/__main__.py
+++ nordicsemi/__main__.py
@@ -468,7 +468,6 @@ def pkg():
                    '\n|s140_nrf52_6.0.0|0xA9|'
                    '\n|s140_nrf52_6.1.0|0xAE|',
               type=click.STRING,
-              required=True,
               multiple=True)
 @click.option('--sd-id',
               help='The new SoftDevice ID to be used as --sd-req for the Application update in case the ZIP '
@@ -569,6 +568,9 @@ def generate(zipfile,
     if hw_version == 'none':
         hw_version = None
 
+    if external_app == None:
+        external_app = False
+
     # Convert multiple value into a single instance
     if len(sd_req) > 1:
         click.echo("Please specify SoftDevice requirements as a comma-separated list: --sd-req 0xXXXX,0xYYYY,...")
@@ -617,7 +619,7 @@ def generate(zipfile,
         click.echo("Error: --hw-version required.")
         return
 
-    if sd_req is None:
+    if sd_req is None and external_app is False:
         click.echo("Error: --sd-req required.")
         return
 
@@ -1127,4 +1129,4 @@ def zigbee(file, jlink_snr, channel):
     of.setup_channel()
 
 if __name__ == '__main__':
-    cli()
+    cli(sys.argv[1:])
